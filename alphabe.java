68ca1267fafc49c55ca820f9cfe8f3d99a9861a7
poonkodi1 <poonkodidevaraj2013@gmail.com>
2016-08-24 18:19:17 +0530

    Add files via upload

---
+++ alphabe.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class alphabe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String b;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the");
+		b=s.nextLine();
+		switch(b)
+		{
+			case "a":
+			{
+				System.out.println("alphabet");
+				break;
+			}
+			case "e":
+			{
+				System.out.println("alphabet");
+				break;
+			}
+			case "i":
+			{
+				System.out.println("alphabet");
+				break;
+			}
+			case "o":
+			{
+				System.out.println("alphabet");
+				break;
+			}
+			case "u":
+			{
+				System.out.println("alphabet");
+				break;
+			}
+			case "1":
+			{
+				System.out.println("not");
+				break;
+			}
+			
+			default:
+				System.out.println("alphabet or not");
+				break;
+		}
+		
+		
+
+	}
+
+}
