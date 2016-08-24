63a1a89e52310bffc912586953824078664cf889
poonkodi1 <poonkodidevaraj2013@gmail.com>
2016-08-24 18:01:50 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Vowels {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String b;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the");
+		b=s.nextLine();
+		switch(b)
+		{
+			case "a":
+			{
+				System.out.println("vowel");
+				break;
+			}
+			case "e":
+			{
+				System.out.println("vowel");
+				break;
+			}
+			case "i":
+			{
+				System.out.println("vowel");
+				break;
+			}
+			case "o":
+			{
+				System.out.println("vowel");
+				break;
+			}
+			case "u":
+			{
+				System.out.println("vowel");
+				break;
+			}
+			
+			default:
+				System.out.println("constants");
+				break;
+		}
+		
+		
+
+	}
+
+}
